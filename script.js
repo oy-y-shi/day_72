b0e239e9164d8c861c32216d0cbbd17e351ea5b0
oy-y-shi <oyshi7419@gmail.com>
2024-09-29 16:47:27 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+//function 
+// addition start 
+function addNumber(number1, number2) {
+     const totalNumber = number1 + number2;
+     console.log("Your additional number is = " + totalNumber);
+}
+addNumber(100 , 200);
+// addition end
+
+
+
+
+
+// substraction start 
+function subNumber(number1, number2) {
+     const totalNumber = number1 - number2;
+     console.log("Your substraction number is = " + totalNumber);
+}
+subNumber(400, 200);
+// substraction end
+
+
+
+
+//multiplecation start 
+function multipleNumber(number1, number2) {
+     const totalNumber = number1 * number2;
+     console.log("Your multiplecation number is = " + totalNumber);
+}
+multipleNumber(5, 8);
+//multiplecation end
+
+
+
+
+// dividation start  
+function devideNumber(number1 , number2) {
+     const totalNumber = number1 / number2;
+     console.log("Your dividation number is = " + totalNumber);
+}
+devideNumber(400, 2); 
+// devidation end
+
+
+
+
+
+
+
+
+
+
+
+
